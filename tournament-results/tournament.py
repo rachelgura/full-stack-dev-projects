e278e9b7693e6902aa07fa31cf655d46f96f76d0
Rachel Gura <sparrowdetour@gmail.com>
2016-12-08 10:59:59 -0600

    clean up, bug fix in slq
    bug fix

--- tournament-results/tournament.py
+++ tournament-results/tournament.py
@@ -1,77 +1,64 @@
 #!/usr/bin/env python
-#
+# 
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
-import contextlib
 
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    try:
-        conn = psycopg2.connect("dbname=tournament")
-        return conn
-    except:
-        print("Database Connection Error")
-
-
-@contextlib.contextmanager
-def myCursor():
-    """Function to consolidate db transaction code."""
-    conn = connect()
-    cursor = conn.cursor()
-    try:
-        yield cursor
-    except:
-        print("Database Cursor Error") 
-    else:
-        conn.commit()
-    finally:
-        cursor.close()
-        conn.close()
+    return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    with myCursor() as cursor:
-        cursor.execute("TRUNCATE matches;")
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE FROM matches;")
+    # c.execute("")
+    DB.commit()
+    DB.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    with myCursor() as cursor:
-        cursor.execute("DELETE FROM players;")
-
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE FROM players;")
+    DB.commit()
+    DB.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    with myCursor() as cursor:
-        cursor.execute("SELECT count(p.id) FROM players p \
-            WHERE p.id != '-404';")
-        result = cursor.fetchone()
-    count = result[0]
+    DB = connect()
+    c = DB.cursor()
+    c.execute("SELECT COUNT(*) FROM players")
+    count = c.fetchone() [0]
+    DB.close()
     return count
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
+  
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
+  
     Args:
       name: the player's full name (need not be unique).
     """
-
-    with myCursor() as cursor:
-        query = "INSERT INTO players (name) VALUES (%s);"
-        parameter = (name,)
-        cursor.execute(query, parameter)
+    DB = connect()
+    c = DB.cursor()
+    c.execute("INSERT INTO players VALUES (DEFAULT, %s)", (name,))
+    DB.commit()
+    DB.close()
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
-    The first entry in the list should be the player in first place,
-    or a player tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
         id: the player's unique id (assigned by the database)
@@ -79,11 +66,15 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    DB = connect()
+    c = DB.cursor()
+    c.execute("SELECT player_id, player_name, total_wins, total_matches"
+              " FROM players ORDER BY total_wins;")
+    standings = c.fetchall()
+    DB.close()
 
-    with myCursor() as cursor:
-        cursor.execute("SELECT id, name, wins, mcount FROM standings ")
-        standings = cursor.fetchall()
     return standings
+
 
 
 def reportMatch(winner, loser):
@@ -92,50 +83,32 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    with myCursor() as cursor:
-        query = "INSERT INTO matches (winner_id, loser_id) values (%s, %s);"
-        parameters = (winner, loser)
-        cursor.execute(query, parameters)
+    DB = connect()
+    c = DB.cursor()
 
+    # update the matches table
+    c.execute("INSERT INTO matches (winner, loser)"
+              "VALUES (%s, %s);", (winner, loser))
+    DB.commit()
 
-def reportBye(bye_player):
-    """Registers a bye for a given player. Bye id assigned as '-404'.
-    Updates both players table and matches table.
-    Args:
-      bye_player: player id receiving a bye.
-    """
+    #update the players table
+    c.execute("UPDATE players SET total_wins = total_wins + 1, total_matches = total_matches +1"
+              "WHERE player_id = %s", (winner, ))
+    c.execute("UPDATE players SET total_losses = total_losses + 1, total_matches = total_matches +1"
+              "WHERE player_id = %s", (loser, ))
+    DB.commit()
+    DB.close()
 
-    with myCursor() as cursor:
-        query = "INSERT INTO players (id, name) VALUES ('-404','bye');"
-        cursor.execute(query)    
-        query = "INSERT INTO matches (winner_id, loser_id) \
-                values ('%s', '-404');"
-        parameter = (bye_player,)
-        cursor.execute(query, parameter)
-
-
-def hasByes(player_id):
-    """Checks if a player has byes checking the DB for bye ids '-404'
-    Args:
-      player_id: player id of player in question
-    Returns: count of byes of player
-    """
-
-    with myCursor() as cursor:
-        query = "SELECT count(id) FROM matches WHERE winner_id = %s \
-                        and loser_id ='-404';"
-        parameter = (player_id,)
-        byecount = cursor.execute(query, parameter)
-    return byecount
-
-
+ 
+ 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
+  
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-    In cases where a bye is needed, bye is assigned id:'-404' name:'bye'
+  
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -143,27 +116,24 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
     standings = playerStandings()
-    players = [(row[0], row[1]) for row in standings]
     pairings = []
+    i=0
 
-    # Bye condition for odd numbers of players
-    if len(players) % 2 != 0:
-        bye_index = len(players) - 1
-        bye_player = players.pop()
-        # Identify player without byes to assign a bye to
-        while (hasByes(bye_player[0]) > 0 and bye_index > 0):
-            players.insert(bye_index, byePlayer)
-            bye_index -= 1
-            bye_player = players.pop(bye_index)
-        pairings.insert(
-            bye_index, (bye_player[0], bye_player[1], '-404', 'bye'))
-
-    # Assign players pairings
-    while (len(players) > 1):
-        player1 = players.pop()
-        player2 = players.pop()
-        pairings.insert(0, (player1[0], player1[1], player2[0], player2[1]))
+    while i < len(standings):
+        plyr1_id = standings[i][0]
+        plyr1_name = standings[i][1]
+        plyr2_id = standings[i+1][0]
+        plyr2_name = standings[i+1][1]
+        pairings.append((plyr1_id, plyr1_name, plyr2_id, plyr2_name))
+        i=i+2
 
     return pairings
+
+def registerTournament(tournament_name):
+    """
+     Adds a tournament to the tournament database
+     Each tournament is assigned a unique serial id number as primary key
+    :param tournament_name: a text field to capture a descriptive name of the
+                            tournament
+    """
--- tournament-results/tournament.sql
+++ tournament-results/tournament.sql
@@ -6,43 +6,39 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-DROP DATABASE IF EXISTS tournament;
+
+
+--create the database
+DROP DATABASE tournament;
 CREATE DATABASE tournament;
+
+--connect to database
 \c tournament
 
+--create the tournament table
+CREATE TABLE tournament (
+  tournament_id SERIAL PRIMARY KEY, tournament_name TEXT
+);
+
+-- create the players table
 CREATE TABLE players (
-	id serial primary key,
-	name varchar(40)
+  player_id SERIAL PRIMARY KEY, player_name TEXT, total_wins INTEGER NOT NULL DEFAULT 0,
+  total_losses INTEGER NOT NULL DEFAULT 0, total_matches INTEGER NOT NULL DEFAULT 0
 );
 
+--create tournament register
+CREATE TABLE tournament_register (
+  register_id SERIAL PRIMARY KEY,
+  tournament_id INTEGER NOT NULL REFERENCES tournament(tournament_id),
+  entrant_id INTEGER NOT NULL REFERENCES players(player_id),
+  wins INTEGER NOT NULL DEFAULT 0,
+  matches INTEGER NOT NULL DEFAULT 0
+);
+
+--create the matches table
 CREATE TABLE matches (
-	id serial primary key,
-	winner_id int references players(id),
-	loser_id int references players(id)
+  match_id SERIAL PRIMARY KEY,
+  --tournament_id INTEGER NOT NULL REFERENCES tournament(tournament_id),
+  winner INTEGER references players(player_id), 
+  loser INTEGER references players(player_id)
 );
-
-CREATE VIEW losers AS (
-	SELECT loser_id, count(id) as losses
-	FROM matches
-	GROUP BY loser_id
-	ORDER BY losses DESC
-);
-
-CREATE VIEW stats AS (
-	SELECT p.id, p.name, count(m.winner_id) AS wins, count (m.id)  AS mcount
-	FROM players p
-	LEFT JOIN matches m ON p.id = m.winner_id 
-	GROUP BY p.id 
-	UNION
-	SELECT p.id, p.name, 0 AS wins, count (m.loser_id) AS mcount
-	FROM players p, losers m
-	WHERE p.id = m.loser_id
-	GROUP BY p.id
-	ORDER BY wins DESC
-);
-
-CREATE VIEW standings AS (
-	SELECT id, name, SUM(wins) as wins, SUM(mcount) as mcount 
-	FROM stats
-	GROUP BY id, name
-	ORDER BY wins DESC
